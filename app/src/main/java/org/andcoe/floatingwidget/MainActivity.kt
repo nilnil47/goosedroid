58afc96fc2d0af117c3436fc8b70837836a09a09
Neil <nilnil47@gmail.com>
2021-05-24 20:33:37 +0300

    first version!!!

--- app/src/main/java/org/andcoe/floatingwidget/MainActivity.kt
+++ app/src/main/java/org/andcoe/floatingwidget/MainActivity.kt
@@ -1,7 +1,11 @@
 package org.andcoe.floatingwidget
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,6 +17,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (!isDrawOverlaysAllowed()) {
+            requestForDrawingOverAppsPermission()
+        }
         finish()
     }
+
+    private fun isDrawOverlaysAllowed(): Boolean =
+        Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)
+
+    private fun requestForDrawingOverAppsPermission() {
+        val intent =
+            Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+        startActivityForResult(intent, DRAW_OVERLAYS_PERMISSION_REQUEST_CODE)
+    }
+
+//    private fun startFloatingWidgetMaybe() {
+//        if (isDrawOverlaysAllowed()) {
+//            startService(Intent(this@MainActivity, FloatingWidgetService::class.java))
+//            return
+//        }
+//    }
+
+
+
 }
